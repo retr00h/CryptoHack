94d86750ba27de1db2592cfd5b642abe588e6b5f
Fabio <fabio.cire.156@gmail.com>
2021-08-02 18:03:23 +0200

    1_Greatest_Common_Divisor

---
+++ 1_General/3_Mathematics/1_Greatest_Common_Divisor.py
@@ -0,0 +1,30 @@
+# The Greatest Common Divisor (GCD), sometimes known as the highest common factor,
+# is the largest number which divides two positive integers (a,b).
+
+# For a = 12, b = 8 we can calculate the divisors of a: {1,2,3,4,6,12}
+# and the divisors of b: {1,2,4,8}. Comparing these two, we see that gcd(a,b) = 4.
+
+# We say that for any two integers a,b, if gcd(a,b) = 1 then a and b are coprime integers.
+
+# If a and b are prime, they are also coprime.
+# If a is prime and b < a then a and b are coprime.
+
+# Think about the case for a prime and b > a, why are these not necessarily coprime?
+
+# There are many tools to calculate the GCD of two integers,
+# but for this task we recommend looking up Euclid's Algorithm.
+# (https://en.wikipedia.org/wiki/Euclidean_algorithm)
+
+# Try coding it up; it's only a couple of lines. Use a = 12, b = 8 to test it.
+
+# Now calculate gcd(a,b) for a = 66528, b = 52920 and enter it below.
+
+def gcd(a, b):
+	q = a // b
+	r = a % b
+	if r == 0: return b
+	else: return gcd(b, r)
+
+a = 66528
+b = 52920
+print("gcd(" + str(a) + ", " + str(b) + ") = " + str(gcd(a, b)))
