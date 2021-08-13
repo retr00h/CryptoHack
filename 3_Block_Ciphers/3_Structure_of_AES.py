76b7ff2458b1f43c1ea3c1a0dbf36e055d12101c
Fabio <fabio.cire.156@gmail.com>
2021-08-13 17:35:04 +0200

    Block ciphers: Keyed permutations, Resisting bruteforce

---
+++ 3_Block_Ciphers/3_Structure_of_AES.py
@@ -0,0 +1,7 @@
+# To acheive a keyed permutation that is infeasible to invert without the key,
+# AES applies a large number of ad-hoc mixing operations on the input.
+# This is in stark contrast to public-key cryptosystems like RSA, which
+# are based on elegant individual mathematical problems.
+# AES is much less elegant, but it's very fast.
+
+
